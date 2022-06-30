2c58fae140696722655d5544a5bc029d6411a0f4
ruman649 <mruman649@gmail.com>
2022-06-30 14:17:20 +0600

    fix js issue

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
+console.log(
+    'Hello word'
+)
+
 let html = document.querySelector('html');
 let colorCode = document.querySelector('#box');
+const trigger = document.querySelector('#actionBtn');
 
-function func(){
+trigger.addEventListener('click', func)
+
+function func() {
+    console.log('trigger')
     let color = colorCode.value;
-    if(color.includes(',') && !color.includes('%')){
+    if (color.includes(',') && !color.includes('%')) {
         html.style.background = `rgb(${color})`;
     }
-    else if (color.includes('%')){
+    else if (color.includes('%')) {
         html.style.background = `hsl(${color})`;
     }
-    else{
+    else {
         html.style.background = color;
     }
 }
